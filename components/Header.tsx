98545fe8559c7305793e4977e3caf6717eb79dec
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-01 18:27:15 +0900

    add SearchBar and Avatar to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,30 @@
 import Image from "next/image";
+import Avatar from "@/components/Avatar";
+import SearchBar from "@/components/SearchBar";
 
 const Header = () => {
   return (
     <header>
-      {/* Logo */}
-      <Image
-        src="/images/trello-logo.svg"
-        alt="trello-logo"
-        width={200}
-        height={100}
-        className="w-44 pb-10 md:w-56 md:pb-0 object-contain"
-      />
+      <div
+        className="flex flex-col md:flex-row items-center 
+        p-5 bg-gray-500/10 rounded-b-2xl
+        "
+      >
+        {/* Logo */}
+        <Image
+          src="/images/trello-logo.svg"
+          alt="trello-logo"
+          width={200}
+          height={100}
+          className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+        />
 
-      <div className="flex items-center">
-        {/* Search Box */}
-        {/* Avatar */}
+        <div className="flex items-center flex-1 justify-end space-x-5">
+          {/* Search Bar*/}
+          <SearchBar />
+          {/* Avatar */}
+          <Avatar />
+        </div>
       </div>
     </header>
   );
