c0e05a93acaa99d08294c888a6499275ee5d496b
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-04 20:15:51 +0900

    add example of data fetched from appwrite in getTodosGroupedByStatus.ts

--- service/getTodosGroupedByStatus.ts
+++ service/getTodosGroupedByStatus.ts
@@ -3,8 +3,26 @@ import { databases } from "@/appwrite";
 export const getTodosGroupedByStatus = async () => {
   const response = await databases.listDocuments(
     process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID,
-    process.env.NEXT_PUBLIC_APPWRITE_TODOS_COLLECTION_ID, 
+    process.env.NEXT_PUBLIC_APPWRITE_TODOS_COLLECTION_ID
   );
 
-  console.log(response);
+  // console.log(response);
+  // Models.DocumentList: {
+  //  "documents": Models.Document[],
+  //  "total": number,
+  // }
+  // Models.Document: {
+  // $id: "64a3f5798fbf0de217af"
+  // $databaseId: "64a2826e36ff54efc383"
+  // $collectionId: "64a283667d439ae9c223"
+  // $permissions: []
+  // $createdAt: "2023-07-04T10:33:29.589+00:00"
+  // $updatedAt: "2023-07-04T10:33:29.589+00:00"
+  // title: "Test Task 1"
+  // image: null
+  // status: "todo"
+  // }
+  //
+  return response;
 };
+
