b17df57b803100283b87b8b25a94cda43a9f4129
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-04 20:21:59 +0900

    add useEffect to call fetchBoard of boardStore to check the data as a trial in Board/index

--- app/components/Board/index.tsx
+++ app/components/Board/index.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import { useEffect } from "react";
+import { useBoardStore } from "@/store/boardStore";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 const Board = () => {
+  const { fetchBoard } = useBoardStore();
+
+  useEffect(() => {
+    fetchBoard();
+  }, [fetchBoard])
+
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div></div>}
-      </Droppable>
-    </DragDropContext>
+    <div>Board</div>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div></div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 };
 
