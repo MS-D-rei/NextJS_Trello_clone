be3e72c97e5ae1e3d0ef81bd49a0f13e9cd23e4d
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-04 20:19:43 +0900

    add fetchBoard method to call getTodosGroupedByStatus and set the data to Board in boardStore.ts

--- store/boardStore.ts
+++ store/boardStore.ts
@@ -1,12 +1,17 @@
 import { create } from "zustand";
 import { Board, BoardColumn, StatusType } from "@/types/board-type";
+import { getTodosGroupedByStatus } from "@/service/getTodosGroupedByStatus";
 
 interface BoardState {
   board: Board;
+  fetchBoard: () => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
   board: {
     columns: new Map<StatusType, BoardColumn>(),
   },
+  fetchBoard: async () => {
+    const todos = getTodosGroupedByStatus();
+  }
 }));
