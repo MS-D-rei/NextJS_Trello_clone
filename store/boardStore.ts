14bd2a8cc0315a13a275a41a73707ffcee59157b
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-08 17:22:56 +0900

    change how to manage board state in store/boardStore.ts

--- store/boardStore.ts
+++ store/boardStore.ts
@@ -1,18 +1,53 @@
 import { create } from "zustand";
-import { Board, BoardColumn, StatusType } from "@/types/board-type";
-import { getTodosGroupedByStatus } from "@/service/getTodosGroupedByStatus";
+import { ColumnData, TodoData } from "@/types/board-type";
+import { getColumnAndTodoData } from "@/service/getColumnAndTodoData";
+import { arrayMove } from "@dnd-kit/sortable";
 
 interface BoardState {
-  board: Board;
+  columns: ColumnData;
+  todos: TodoData;
   fetchBoard: () => void;
+  changeColumnOrder: (activeId: string, overId: string) => void;
 }
 
 export const useBoardStore = create<BoardState>((set) => ({
-  board: {
-    columns: new Map<StatusType, BoardColumn>(),
+  columns: {
+    byId: {
+      todo: { id: "todo", todoIds: [] },
+      "in-progress": { id: "in-progress", todoIds: [] },
+      done: { id: "done", todoIds: [] },
+    },
+    allIds: [],
+  },
+  todos: {
+    byId: {},
+    allIds: [],
   },
   fetchBoard: async () => {
-    const board = await getTodosGroupedByStatus();
-    set({ board });
+    const { columnData, todoData } = await getColumnAndTodoData();
+
+    set({ columns: columnData, todos: todoData });
+  },
+  changeColumnOrder: (activeId, overId) => {
+    if (activeId === overId) return;
+
+    if (activeId !== overId) {
+      set((state) => {
+        const activeIndex = state.columns.allIds.indexOf(activeId);
+        const overIndex = state.columns.allIds.indexOf(overId);
+        const newColumnOrder = arrayMove(
+          state.columns.allIds,
+          activeIndex,
+          overIndex
+        );
+
+        return {
+          columns: {
+            ...state.columns,
+            allIds: newColumnOrder,
+          },
+        };
+      });
+    }
   },
 }));
