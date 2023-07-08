ccbe356367be3583a83f73b46576a446b354640f
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-08 17:29:54 +0900

    add SortableColumn to Board/index.tsx

--- app/components/Board/index.tsx
+++ app/components/Board/index.tsx
@@ -1,37 +1,54 @@
 "use client";
 
 import { useEffect } from "react";
-import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import { useBoardStore } from "@/store/boardStore";
-import Column from "@/app/components/Column";
+import {
+  DndContext,
+  DragEndEvent,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { SortableContext } from "@dnd-kit/sortable";
+import SortableColumn from "@/app/components/Board/SortableColumn";
 
 const Board = () => {
-  const { board, fetchBoard } = useBoardStore();
+  const { columns, todos, fetchBoard, changeColumnOrder } = useBoardStore();
 
   useEffect(() => {
     fetchBoard();
   }, [fetchBoard]);
 
-  console.log(board);
+  console.log(columns);
+  console.log(todos);
 
-  const handleDragEnd = (result: DropResult) => { };
+  const sensors = useSensors(
+    useSensor(KeyboardSensor),
+    useSensor(PointerSensor)
+  );
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    console.log(event);
+
+    if (event.over === null) return;
+
+    const activeId = event.active.id as string;
+    const overId = event.over.id as string;
+
+    changeColumnOrder(activeId, overId);
+  };
 
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => (
-          <div
-            {...provided.droppableProps}
-            ref={provided.innerRef}
-            className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto"
-          >
-            {Array.from(board.columns.entries()).map(([id, column], index) => (
-              <Column key={id} id={id} index={index} todos={column.todos} />
-            ))}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
+      <SortableContext id="board" items={columns.allIds}>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto">
+          {columns.allIds.map((status) => (
+            <SortableColumn key={status} id={status} />
+          ))}
+        </div>
+      </SortableContext>
+    </DndContext>
   );
 };
 
