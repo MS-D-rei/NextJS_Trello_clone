21e1e72c3b33d20cc9a8ec62de23968a17315763
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-22 22:55:00 +0900

    modify css in components/Modal.tsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -23,11 +23,11 @@ const Button: React.FC<ButtonProps> = ({
       disabled={disabled}
       onClick={onClick}
       className={clsx(
-        `flex justify-center rounded-md text-sm font-semibold text-white p-2`,
+        `flex justify-center rounded-md text-sm font-semibold p-2`,
         disabled && "opacity-50 cursor-default",
-        secondary && "bg-gray-400 hover:bg-gray-500",
-        danger && "bg-rose-500 hover:bg-rose-600",
-        !secondary && !danger && "bg-sky-500 hover:bg-sky-600"
+        secondary && "text-gray-700 bg-white hover:bg-gray-50 ring-1 ring-inset ring-gray-300",
+        danger && "text-white bg-rose-500 hover:bg-rose-600",
+        !secondary && !danger && "text-white bg-sky-500 hover:bg-sky-600"
       )}
     >
       {children}
--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,31 +1,72 @@
-'use client';
+"use client";
 
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import { useModalStore } from "@/store";
 
-const Modal = () => {
+interface ModalProps {
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ children }) => {
   const { isModalOpen, closeModal } = useModalStore();
-  
-  console.log('isModalOpen', isModalOpen);
+
   return (
-    <Transition
-      show={isModalOpen}
-      enter="transition duration-100 ease-out"
-      enterFrom="transform scale-95 opacity-0"
-      enterTo="transform scale-100 opacity-100"
-      leave="transition duration-75 ease-out"
-      leaveFrom="transform scale-100 opacity-100"
-      leaveTo="transform scale-95 opacity-0"
-      as={Fragment}
-    >
-      <Dialog onClose={closeModal}>
-        <Dialog.Panel>
-          <Dialog.Title>Deactivate account</Dialog.Title>
-          {/* ... */}
-        </Dialog.Panel>
+    <Transition.Root show={isModalOpen} as={Fragment}>
+      <Dialog as="div" onClose={closeModal} className="relative z-50">
+        {/* gray schreen */}
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div
+            className="fixed inset-0 bg-gray-500/75 transition-opacity"
+            aria-hidden={true}
+          />
+        </Transition.Child>
+
+        {/* Full-screen scrollable container */}
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          {/* Container to center the panel */}
+          <div className="flex min-h-full items-center justify-center p-4">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Panel
+                className="transform overflow-hidden bg-white 
+              rounded-lg text-left shadow-xl transition-all 
+              sm:my-8 sm:w-full sm:max-w-lg"
+              >
+                {/* <div className="flex justify-end">
+                  <button
+                    type="button"
+                    onClick={closeModal}
+                    className="rounded-md bg-white text-gray-400 hover:text-gray-500
+                    focus:outline-none p-2"
+                  >
+                    <span className="sr-only">Close</span>
+                    <XMarkIcon className="h-6 w-6" />
+                  </button>
+                </div> */}
+                {children}
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
       </Dialog>
-    </Transition>
+    </Transition.Root>
   );
 };
 
