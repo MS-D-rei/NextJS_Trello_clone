ccbf967a3d552b93f746c7c33ec564c586fa6d24
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-22 14:03:19 +0900

    add secondary attribute to components/Button.tsx

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,29 +1,38 @@
 import clsx from "clsx";
 
 interface ButtonProps {
-  type: 'button' | 'submit' | 'reset' | undefined;
-  disabled?: boolean;
+  type: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
+  disabled?: boolean;
+  secondary?: boolean;
   danger?: boolean;
   children?: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ type, disabled, onClick, danger, children }) => {
+const Button: React.FC<ButtonProps> = ({
+  type,
+  onClick,
+  disabled,
+  secondary,
+  danger,
+  children,
+}) => {
   return (
     <button
       type={type}
       disabled={disabled}
       onClick={onClick}
       className={clsx(
-        `flex justify-center rounded-md text-sm font-semibold`,
-        disabled && 'opacity-50 cursor-default',
-        danger && 'bg-rose-500 hover:bg-rose-600',
-        !danger && 'bg-sky-500 hover:bg-sky-600'
-        )}
+        `flex justify-center rounded-md text-sm font-semibold text-white p-2`,
+        disabled && "opacity-50 cursor-default",
+        secondary && "bg-gray-400 hover:bg-gray-500",
+        danger && "bg-rose-500 hover:bg-rose-600",
+        !secondary && !danger && "bg-sky-500 hover:bg-sky-600"
+      )}
     >
       {children}
     </button>
-  )
+  );
 };
 
 export default Button;
