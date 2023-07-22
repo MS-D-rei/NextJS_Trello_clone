7bfba420d456ce31f1f30a7b4f115b2add48f8b4
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-23 00:03:38 +0900

    add ConfirmModal to app/(site)/page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,18 +1,15 @@
 import Header from "@/app/(site)/components/Header";
 import Board from "@/app/(site)/components/Board";
-import Modal from "@/app/components/Modal";
+import ConfirmModal from "../components/ConfirmModal";
 
 export default function Home() {
   return (
     <main>
-      {/* Header */}
       <Header />
 
-      {/* Board */}
       <Board />
 
-      {/* Modal */}
-      <Modal />
+      <ConfirmModal />
     </main>
   )
 }
