c70167dd38de239b34906dd1bf343b52774e7fa6
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-07-28 19:12:57 +0900

    modify upload button appearance in ImageUpload.tsx

--- app/components/ImageUpload.tsx
+++ app/components/ImageUpload.tsx
@@ -4,6 +4,7 @@ import { ChangeEvent, useRef } from "react";
 import Image from "next/image";
 import { useSelectFile } from "@/app/hooks/useSelectFile";
 import Button from "@/app/components/Button";
+import { PhotoIcon } from "@heroicons/react/24/solid";
 
 const ImageUpload = () => {
   const selectedFileRef = useRef<HTMLInputElement>(null);
@@ -15,6 +16,7 @@ const ImageUpload = () => {
   };
 
   const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log("handleImageEvent", event);
     selectFile(event);
   };
 
@@ -23,25 +25,33 @@ const ImageUpload = () => {
   };
 
   return (
-    <div className="flex items-center justify-center w-full">
+    <div>
       {selectedFile ? (
-        <>
+        <div className="flex flex-col gap-1">
           <Image
             src={selectedFile}
             alt="selectedImage"
-            width={100}
-            height={100}
+            width={300}
+            height={200}
             priority={false}
           />
           <Button type="button" secondary onClick={handleImageCancel}>
             cancel
           </Button>
-        </>
+        </div>
       ) : (
         <div>
-          <Button type="button" onClick={handleUpload}>
-            Upload
-          </Button>
+          <button
+            type="button"
+            onClick={handleUpload}
+            className="flex items-center justify-center w-full h-44
+            border rounded-md shadow-md gap-1 hover:bg-gray-100"
+          >
+            <PhotoIcon className="w-6 h-6" />
+            <span className="text-sm font-semibold text-gray-600">
+              Select Image
+            </span>
+          </button>
           <input
             type="file"
             ref={selectedFileRef}
